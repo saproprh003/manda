d5bfef56c12758d00d17653b718eb5faa5a91490
Hackerphanto <San99h5k@gmail.com>
2023-09-30 13:46:21 +0530

    Create Hello_World.py

---
+++ Hello_World.py
@@ -0,0 +1,5 @@
+#In this file I am writing how to print the user input times "Hello world"
+n=int(input("Hello world")) #Taking the input from the user and storing it in the variable n
+#I am looping the Hello world from the 0 to n=(user input times)
+for i in range(0,n):
+    print("Hello World")
